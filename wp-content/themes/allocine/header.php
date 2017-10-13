a9a25a633386ea87dd023af4883052ec1ef8b989
gregoirepuget <gregoire@36pixels.fr>
2017-10-13 10:41:13 +0200

    header

--- wp-content/themes/allocine/header.php
+++ wp-content/themes/allocine/header.php
@@ -15,6 +15,15 @@
     </head>
     <body <?php body_class(); ?>>
         <header id="header">
-        <!-- Tout le contenu de l entête de mon site -->
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12">
+                <img src="<?= IMAGES_URL;?>/logo.png" class="logo" alt="logo du site <?php bloginfo( 'title' ); ?>"/>
+                <div class="site_information">
+                  <h2><a href="<?php bloginfo( 'url' ); ?>" title="<?php bloginfo( 'title' ); ?>"><?php bloginfo( 'title' ); ?></a></h2>
+                  <span><?php bloginfo( 'description' ); ?></span>
+                </div>
+              </div>
+            </div>
+          </div>
         </header>
-        
