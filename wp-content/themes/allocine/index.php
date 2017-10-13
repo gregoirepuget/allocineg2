7856e4edde1e91d4ce568085f2da9ff815f95b53
gregoirepuget <gregoire@36pixels.fr>
2017-10-13 10:59:38 +0200

    modif index.php

--- wp-content/themes/allocine/index.php
+++ wp-content/themes/allocine/index.php
@@ -1,16 +1,30 @@
 <?php get_header(); //appel du template header.php  ?>
 
-<div id="content">
-    <h1>Contenu Principal</h1>
+<div id="content" class="container">
+  <div class="row">
+    <h1 class="col-sm-12">Contenu Principal</h1>
+  </div>
+  <div class="row">
     <?php
     // boucle WordPress
     if (have_posts()){
         while (have_posts()){
             the_post();
     ?>
-            <h1><?php the_title(); ?></h1>
-            <h2>Posté le <?php the_time('F jS, Y') ?></h2>
-            <p><?php the_content(); ?></p>
+      <article class="col-sm-12 col-md-4">
+        <div class="thumbnail">
+          <?php
+            if(has_post_thumbnail())
+            {
+              the_post_thumbnail("full");
+            }
+          ?>
+        </div>
+        <h1><?php the_title(); ?></h1>
+        <h2>Posté le <?php the_time('F jS, Y') ?></h2>
+        <p><?php the_excerpt(); ?></p>
+      </article>
+
     <?php
     }
     }
@@ -20,6 +34,7 @@
     <?php
     }
     ?>
+  </div>
 </div> <!-- /content -->
 
 <?php get_footer(); //appel du template footer.php ?>
